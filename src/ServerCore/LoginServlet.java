e2aa39654f1c5c19fb87914129ea6d849a493ee6
debian <debian@vm-debian.tomato-router>
2012-12-19 10:02:00 +0100

    Servlet

---
+++ src/ServerCore/LoginServlet.java
@@ -0,0 +1,48 @@
+package ServerCore;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoginServlet() {
+        super();
+        
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		RequestDispatcher rd = request.getRequestDispatcher("content/login.html");
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		String login = request.getParameter("login");
+		String password = request.getParameter("password");
+		
+		if ((login != null) && (password != null))
+		{
+			if (CredentialsMatch(login,password))
+			{
+				RequestDispatcher rd = request.getRequestDispatcher("contacts.html");
+				rd.forward(request, response);
+			}
+		}
+	}
+	
+	private boolean CredentialsMatch(String login, String password)
+	{
+		return true;
+	}
+
+}
