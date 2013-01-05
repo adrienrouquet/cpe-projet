fd541fb47727fc4167031d4b5cccb74ba886c904
debian <debian@vm-debian.tomato-router>
2013-01-05 15:34:05 +0100

    mise en "function"

--- WebContent/script/websocketChat.js
+++ WebContent/script/websocketChat.js
@@ -35,10 +35,17 @@ $(document).ready(function() {
 //		outgoingMsg.find(".messageStatus").html('X');
 		writeNewMessage(outgoingMsg, json);
 		
-		$(".section").animate({scrollTop: $(".section").prop("scrollHeight")}, 0);
+		scrollDown();
 		
 		var message = json.stringify();
 		_websocket.emit("sendMessage", message);
+	}
+	
+	function scrollDown() {
+		$(".section").animate({
+			scrollTop: $(".section").prop("scrollHeight")
+			},
+			0);
 	}
 	
 	function writeNewMessage(element, json) {
